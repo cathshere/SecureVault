f4a1785643bcd2b3d5cde196424eb8d4f572f86a
Catherine Abhiroop <telsamariecatherine@gmail.com>
2026-06-30 11:06:32 +0530

    Update phishing_keywords.py

--- backend/cybersecurity/phishing_det/phishing_keywords.py
+++ backend/cybersecurity/phishing_det/phishing_keywords.py
@@ -1,31 +1,88 @@
-# phishing_keywords.py
-
 def detect_keywords(text):
 
     text = text.lower()
 
     score = 0
-
     tactics = []
 
-    if "urgent" in text:
-        score += 15
+    # Urgency
+    urgency_words = [
+        "urgent",
+        "immediately",
+        "24 hours",
+        "within 24 hours",
+        "as soon as possible",
+        "expires today"
+    ]
+
+    if any(word in text for word in urgency_words):
+        score += 20
         tactics.append("Urgency")
 
-    if "verify" in text:
-        score += 15
+    # Credential Theft
+    credential_words = [
+        "verify",
+        "confirm",
+        "login",
+        "sign in",
+        "account",
+        "credentials"
+    ]
+
+    if any(word in text for word in credential_words):
+        score += 20
         tactics.append("Credential Theft")
 
-    if "password" in text:
-        score += 15
+    # Password Harvesting
+    password_words = [
+        "password",
+        "reset password",
+        "update password",
+        "change password"
+    ]
+
+    if any(word in text for word in password_words):
+        score += 20
         tactics.append("Password Harvesting")
 
-    if "click here" in text:
+    # Click Bait
+    click_words = [
+        "click here",
+        "click below",
+        "open link",
+        "follow this link"
+    ]
+
+    if any(word in text for word in click_words):
         score += 15
         tactics.append("Click Bait")
 
-    if "suspended" in text:
+    # Fear / Threat
+    fear_words = [
+        "suspended",
+        "disabled",
+        "locked",
+        "failure",
+        "terminated",
+        "blocked"
+    ]
+
+    if any(word in text for word in fear_words):
         score += 15
         tactics.append("Fear")
 
-    return score, tactics
+    # Brand Impersonation
+    brand_words = [
+        "microsoft",
+        "office 365",
+        "google",
+        "paypal",
+        "amazon",
+        "bank"
+    ]
+
+    if any(word in text for word in brand_words):
+        score += 15
+        tactics.append("Brand Impersonation")
+
+    return min(score, 100), list(set(tactics))
